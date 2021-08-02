d91a3a6cda6de245a0560cdd63088e70a41552e2
stillcold <mr_kernel@163.com>
2021-08-02 23:22:21 +0800

    code: Add simple implement for x-lua rpc.

--- process/xlua/main.lua
+++ process/xlua/main.lua
@@ -1,6 +1,6 @@
 local core = require "sys.core"
 local mysql = require "sys.db.mysql"
-local json = require "sys/json"
+local json = require "sys.json"
 local gateway = require "gateway"
 local pb = require "pb"
 local protoc = require "protoc"
@@ -15,32 +15,29 @@ protoc:load ([==[
     }
 ]==])
 
+
+local Gac2Gas = {}
+local Gas2Gac = {}
 local localId = 0
-local fd2sessionId = {}
 
 local function SendRpc(fd, sessionId, cmd, context)
 	
-	sign = os.time()
+	sign = tostring(os.time())
 
 	local data = {
 		sessionId 	= sessionId,
 		cmd 		= cmd,
 		sign 		= sign,
-		context 	= json.encode(context),
+		context 	= json.encode(context),-- 这里只是为了快速开发直接用了json,其实可以考虑protobuffer.
 	}
 
 	local bytes = assert(pb.encode("rpc", data))
 	socket.write(fd, string.pack("<I4", #bytes)..bytes)
 end
 
-local function Dispatch(fd, sessionId, sign, cmd, context)
+function Gac2Gas:RequestInitRecommend(fd, sessionId, context)
 
-	fd2SessionId[fd] = fd2SessionId[fd] or (localId + 1)
-
-	local sessionId = fd2SessionId[fd]
-
-	firstName =  context.firstName or ""
-
+	local firstName = context
 	local result = {
 		{-360,0,firstName.."锄"},
 		{-180,0,firstName.."禾"},
@@ -50,7 +47,31 @@ local function Dispatch(fd, sessionId, sign, cmd, context)
 	}
 
 	SendRpc(fd, sessionId, "SyncRecommendName", result)
+end
 
+local function Dispatch(fd, str)
+
+	local rpcInfo = pb.decode("rpc",str)
+	if not rpcInfo then 
+		core.log("decode rpc failed")
+		return
+	end
+	local cmd = rpcInfo.cmd
+	local rpcHandler = Gac2Gas[cmd]
+	if not rpcHandler then
+		core.log("rpc handler not found")
+		return
+	end
+
+	local sessionId = rpcInfo.sessionId
+	if not sessionId then
+		localId = localId + 1
+		sessionId = localId
+	end
+
+	core.log("rpc come in, cmd:", cmd, ", sessionId:", sessionId)
+
+	rpcHandler(Gac2Gas, fd, sessionId, rpcInfo.context)
 	return true
 end
 
@@ -62,7 +83,4 @@ core.start(function()
 	gateway.listen(addr, Dispatch)
 	-- require "HttpServer"
 end)
-
-
-
 
